214931f85f89309c7069a333621a213eb67b55f4
dhanushp <dhanushp.work@gmail.com>
2020-05-29 22:00:53 +0530

    Resize header font

--- src/elements/Titles.jsx
+++ src/elements/Titles.jsx
@@ -21,7 +21,7 @@ export const Title = styled.h1`
 `
 
 export const BigTitle = styled.h1`
-  ${tw`text-5xl lg:text-6xl font-serif text-white mb-6 tracking-wide`};
+  ${tw`text-4xl lg:text-6xl font-serif text-white mb-6 tracking-wide`};
   text-shadow: 0 5px 35px rgba(255, 255, 255, 0.15);
 `
 
